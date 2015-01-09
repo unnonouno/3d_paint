78ea69f27abf5e65cf1bbf025fbd8a5d6bc8be15
Yuya Unno <unnonouno@gmail.com>
2015-01-10 00:22:57 +0900

    Initial commit

---
+++ 3dp.js
@@ -0,0 +1,8 @@
+angular.module("App", [])
+.controller("mainController", function ($scope) {
+  $scope.on_move = function($event) {
+    $scope.data = $event.offsetX + ", " + $event.offsetY;
+    $scope.x = $event.offsetX - $event.target.clientWidth * 0.5;
+    $scope.y = $event.offsetY - $event.target.clientHeight * 0.5;
+  }
+});
