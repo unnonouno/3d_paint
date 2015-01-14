7b0757d09092833960c4dd89dbb86d8b5e7eabc7
Yuya Unno <unnonouno@gmail.com>
2015-01-14 14:48:15 +0900

    Fix

--- 3dp.js
+++ 3dp.js
@@ -9,7 +9,7 @@ angular.module("App", [])
   };
 
   $window.addEventListener('devicemotion', function(e) {
-    $scope.x = e.accelerationIncludingGravity.x * 100;
+    $scope.x = e.accelerationIncludingGravity.x * 1000;
     $scope.$apply();
   });
 }]);
