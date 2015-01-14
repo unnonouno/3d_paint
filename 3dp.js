76e08409bb07a3c28d2cd7ee22d3025e48a36e79
Yuya Unno <unnonouno@gmail.com>
2015-01-15 02:04:40 +0900

    Fix

--- 3dp.js
+++ 3dp.js
@@ -1,6 +1,9 @@
 angular.module("App", [])
 .controller("mainController", ['$scope', '$window', function ($scope, $window) {
+  $scope.xs = [0, 0, 0, 0];
+  $scope.ys = [0, 0, 0, 0];
   $scope.x = 0;
+  $scope.y = 0;
 
   $scope.on_move = function($event) {
     $scope.data = $event.offsetX + ", " + $event.offsetY;
@@ -9,7 +12,23 @@ angular.module("App", [])
   };
 
   $window.addEventListener('devicemotion', function(e) {
-    $scope.x = e.accelerationIncludingGravity.x * 100.0;
+    var x = 0;
+    var y = 0
+    for (var i = 0; i < 3; i++) {
+      $scope.xs[i] = $scope.xs[i + 1];
+      $scope.ys[i] = $scope.ys[i + 1];
+      x += $scope.xs[i];
+      y += $scope.ys[i];
+    }
+    $scope.xs[3] = -e.accelerationIncludingGravity.x * 100.0;
+    $scope.ys[3] = -e.accelerationIncludingGravity.y * 100.0;
+
+    x += $scope.xs[3];
+    y += $scope.ys[3];
+
+    $scope.x = x / 4;
+    $scope.y = y / 4;
+
     $scope.$apply();
   });
 }]);
